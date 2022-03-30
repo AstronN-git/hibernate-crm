9e93c1c76603d85dc439cc20a02cb1a3e72922ef
Maxim <max.sherb2007@gmail.com>
2022-03-30 22:13:42 +0300

    Testing @AfterReturning Aspects + JoinPoints

--- src/main/java/com/max/aspect/LoggingAspect.java
+++ src/main/java/com/max/aspect/LoggingAspect.java
@@ -1,23 +1,41 @@
 package com.max.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import com.max.entity.User;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Aspect
 @Component
 public class LoggingAspect {
     @Pointcut("execution(* com.max.dao.*.*(..))")
-    public void DAOPointCut() {}
+    public void daoPointcut() {}
 
-    @Before("DAOPointCut()")
-    public void beforeSavingUserAdvice() {
-        System.out.println("> @Before executed");
+    @Pointcut("execution(* com.max.dao.*.get*(..))")
+    public void daoGetMethodsPointcut() {}
+
+    @Pointcut("execution(java.util.List com.max.dao.UserDAO.getUsers())")
+    public void daoListUsers() {}
+
+    @Before("daoPointcut()")
+    public void beforeDaoMethods(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+
+        System.out.println("> @Before " + signature.toString());
     }
 
-    @Before("DAOPointCut()")
-    public void beforeSavingUserAdvice2() {
-        System.out.println("> @Before 2 executed");
+    @Before("daoPointcut() && !daoGetMethodsPointcut()")
+    public void beforeUpdatingDatabase() {
+        System.out.println("> @Before some updates in db");
+    }
+
+    @AfterReturning(value = "daoListUsers()", returning = "users")
+    public void afterListingUsers(JoinPoint joinPoint, List<User> users) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("> @AfterReturning " + signature.toString() + ":");
+        users.forEach(System.out::println);
     }
 }
